66e55e7ef3c33ac5b528be65872a1151e801332f
A284Philipi <78768392+A284Philipi@users.noreply.github.com>
2021-05-20 19:58:33 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+    int casos, cont = 0;
+    string a;
+    cin >> casos;
+    while (cont <= casos){
+        getline(cin, a);
+        cont++;
+    }
+    cout << "Ciencia da Computacao" <<endl;
+    return 0;
+}
